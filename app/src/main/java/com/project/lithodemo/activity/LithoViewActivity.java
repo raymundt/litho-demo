e0349e7254a234bf30dc8067a82973bf8d208ba7
Raymund Tong <raymundtong@hk01.com>
2017-05-02 16:51:10 +0800

    Add Litho recycler adapter

--- app/src/main/java/com/project/lithodemo/activity/LithoViewActivity.java
+++ app/src/main/java/com/project/lithodemo/activity/LithoViewActivity.java
@@ -7,23 +7,11 @@ import android.support.v7.widget.OrientationHelper;
 
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
-import com.facebook.litho.ComponentInfo;
 import com.facebook.litho.LithoView;
 import com.facebook.litho.widget.LinearLayoutInfo;
 import com.facebook.litho.widget.Recycler;
-import com.facebook.litho.widget.RecyclerBinder;
-import com.project.lithodemo.component.ArticleLargeImageItem;
-import com.project.lithodemo.component.ArticleSmallImageItem;
-import com.project.lithodemo.component.ArticleTextOnlyItem;
-import com.project.lithodemo.component.ArticleThreeImagesItem;
+import com.project.lithodemo.adapter.LithoRecyclerAdapter;
 import com.project.lithodemo.data.SampleData;
-import com.project.lithodemo.object.Article;
-
-import java.util.ArrayList;
-
-import static com.project.lithodemo.object.Article.DisplayType.LARGE_IMAGE;
-import static com.project.lithodemo.object.Article.DisplayType.TEXT_ONLY;
-import static com.project.lithodemo.object.Article.DisplayType.THREE_IMAGES;
 
 public class LithoViewActivity extends AppCompatActivity {
 
@@ -31,49 +19,24 @@ public class LithoViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final ComponentContext context = new ComponentContext(this);
-        final RecyclerBinder recyclerBinder = new RecyclerBinder(context, new LinearLayoutInfo(this, OrientationHelper.VERTICAL, false));
-        final Component component = Recycler.create(context)
-                .binder(recyclerBinder)
+        final ComponentContext componentContext = new ComponentContext(this);
+
+        //  Adapter (RecyclerBinder) setup
+        final LithoRecyclerAdapter adapter = new LithoRecyclerAdapter(
+                componentContext,
+                new LinearLayoutInfo(this, OrientationHelper.VERTICAL, false));
+
+        //  Recycler setup
+        final Component component = Recycler.create(componentContext)
+                .binder(adapter)
+                //  Add divider
                 .itemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
                 .build();
 
-        ArrayList<Article> data = new SampleData().get();
-        for (int i = 0; i < data.size(); i++) {
-            Article article = data.get(i);
-            ComponentInfo.Builder componentInfoBuilder = ComponentInfo.create();
-            if (data.get(i).getDisplayType() == TEXT_ONLY) {
-                componentInfoBuilder.component(
-                        ArticleTextOnlyItem.create(context)
-                                .title(article.getTitle())
-                                .build());
-            }
-            else if (data.get(i).getDisplayType() == LARGE_IMAGE) {
-                componentInfoBuilder.component(
-                        ArticleLargeImageItem.create(context)
-                                .title(article.getTitle())
-                                .image(article.getImage())
-                                .build());
-            }
-            else if (data.get(i).getDisplayType() == THREE_IMAGES) {
-                componentInfoBuilder.component(
-                        ArticleThreeImagesItem.create(context)
-                                .title(article.getTitle())
-                                .images(article.getImages())
-                                .build());
-            }
-            else {
-                componentInfoBuilder.component(
-                        ArticleSmallImageItem.create(context)
-                                .title(article.getTitle())
-                                .image(article.getImage())
-                                .aspectRatio(1)
-                                .build());
-            }
-            recyclerBinder.insertItemAt(i, componentInfoBuilder.build());
-        }
+        //  Set data
+        adapter.setItems(new SampleData().get());
 
-        setContentView(LithoView.create(context, component));
+        setContentView(LithoView.create(componentContext, component));
     }
 
 }
---
+++ app/src/main/java/com/project/lithodemo/adapter/LithoRecyclerAdapter.java
@@ -0,0 +1,70 @@
+package com.project.lithodemo.adapter;
+
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.ComponentInfo;
+import com.facebook.litho.widget.LayoutInfo;
+import com.facebook.litho.widget.RecyclerBinder;
+import com.project.lithodemo.component.ArticleLargeImageItem;
+import com.project.lithodemo.component.ArticleSmallImageItem;
+import com.project.lithodemo.component.ArticleTextOnlyItem;
+import com.project.lithodemo.component.ArticleThreeImagesItem;
+import com.project.lithodemo.object.Article;
+
+import java.util.ArrayList;
+
+import static com.project.lithodemo.object.Article.DisplayType.LARGE_IMAGE;
+import static com.project.lithodemo.object.Article.DisplayType.TEXT_ONLY;
+import static com.project.lithodemo.object.Article.DisplayType.THREE_IMAGES;
+
+public class LithoRecyclerAdapter extends RecyclerBinder {
+
+    private ArrayList<Article> mItems;
+    protected ComponentContext mComponentContext;
+
+    public LithoRecyclerAdapter(ComponentContext c, LayoutInfo layoutInfo) {
+        super(c, 4f, layoutInfo, null, false);
+        mComponentContext = c;
+    }
+
+    public void setItems(ArrayList<Article> items) {
+        if (getItemCount() > 0)
+            removeRangeAt(0, getItemCount());
+
+        mItems = items;
+
+        for (int i = 0; i < mItems.size(); i++) {
+            Article article = mItems.get(i);
+            ComponentInfo.Builder componentInfoBuilder = ComponentInfo.create();
+            if (article.getDisplayType() == TEXT_ONLY) {
+                componentInfoBuilder.component(
+                        ArticleTextOnlyItem.create(mComponentContext)
+                                .title(article.getTitle())
+                                .build());
+            }
+            else if (article.getDisplayType() == LARGE_IMAGE) {
+                componentInfoBuilder.component(
+                        ArticleLargeImageItem.create(mComponentContext)
+                                .title(article.getTitle())
+                                .image(article.getImage())
+                                .build());
+            }
+            else if (article.getDisplayType() == THREE_IMAGES) {
+                componentInfoBuilder.component(
+                        ArticleThreeImagesItem.create(mComponentContext)
+                                .title(article.getTitle())
+                                .images(article.getImages())
+                                .build());
+            }
+            else {
+                componentInfoBuilder.component(
+                        ArticleSmallImageItem.create(mComponentContext)
+                                .title(article.getTitle())
+                                .image(article.getImage())
+                                .aspectRatio(1)
+                                .build());
+            }
+            insertItemAt(i, componentInfoBuilder.build());
+        }
+    }
+
+}
